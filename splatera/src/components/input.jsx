15b84afe224bc9fab024a053a58cf4209ccb5809
Daniil <P01_Koksler@proton.me>
2026-05-31 23:04:39 +0300

    Search redesign

--- splatera/src/components/input.jsx
+++ splatera/src/components/input.jsx
@@ -1,6 +1,18 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './input.css';
 import { Tooltip } from './tooltip';
+import Tag from './Tag';
+import ColorPicker from './colorPicker';
+import {
+  useFloating,
+  autoUpdate,
+  offset,
+  flip,
+  shift,
+  useDismiss,
+  useInteractions,
+  FloatingFocusManager,
+} from '@floating-ui/react';
 
 const Input = React.forwardRef(({ 
   icon: Icon, 
@@ -11,11 +23,52 @@ const Input = React.forwardRef(({
   tooltip,
   hotkey,
   tooltipPosition = 'bottom',
+  showColorPicker = false,
+  pickerColor,
+  onPickerColorChange,
   ...props 
 }, ref) => {
   const tagsRef = useRef(null);
   const inputRef = useRef(null);
-  const [dynamicPadding, setDynamicPadding] = useState(12);
+  const wrapperRef = useRef(null);
+
+  const [dynamicPadding, setDynamicPadding] = useState(10);
+  const [isTagManagerOpen, setIsTagManagerOpen] = useState(false);
+  const [isColorPickerOpen, setIsColorPickerOpen] = useState(false);
+
+  // Set up floating UI for tag manager dropdown
+  const { refs, floatingStyles, context } = useFloating({
+    open: isTagManagerOpen,
+    onOpenChange: setIsTagManagerOpen,
+    placement: 'bottom-end',
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset(10),
+      flip(),
+      shift({ padding: 10 }),
+    ],
+  });
+
+  const dismiss = useDismiss(context);
+  const { getFloatingProps } = useInteractions([dismiss]);
+
+  // Combine wrapper ref with floating-ui reference setter
+  const setRef = (node) => {
+    wrapperRef.current = node;
+    refs.setReference(node);
+    if (typeof ref === 'function') {
+      ref(node);
+    } else if (ref) {
+      ref.current = node;
+    }
+  };
+
+  // Autoclose tag manager if there are less than 3 tags present in search
+  useEffect(() => {
+    if (selectedTags.length < 3 && isTagManagerOpen) {
+      setIsTagManagerOpen(false);
+    }
+  }, [selectedTags, isTagManagerOpen]);
 
   useEffect(() => {
     const el = tagsRef.current;
@@ -23,7 +76,8 @@ const Input = React.forwardRef(({
 
     const update = () => {
       const w = el.offsetWidth;
-      setDynamicPadding(w > 0 ? w + 24 : 12);
+      // Positioned at right: 10px, so paddingRight needs to be container width + 15px
+      setDynamicPadding(w > 0 ? w + 15 : 10);
     };
 
     update();
@@ -31,7 +85,7 @@ const Input = React.forwardRef(({
     const observer = new ResizeObserver(update);
     observer.observe(el);
     return () => observer.disconnect();
-  }, [selectedTags, selectedColors]);
+  }, [selectedTags, selectedColors, showColorPicker]);
 
   useEffect(() => {
     if (!hotkey) return;
@@ -55,46 +109,95 @@ const Input = React.forwardRef(({
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [hotkey]);
 
-
   const inputContent = (
-    <div className="splatera-input-wrapper" ref={ref}>
-      <div className="search-tags-container" ref={tagsRef}>
-        {selectedColors.map((color, idx) => (
+    <div className="splatera-input-wrapper" ref={setRef}>
+      {Icon && <Icon size={15} className="input-icon" />}
+
+      <input 
+        ref={inputRef}
+        className="splatera-input" 
+        style={{ 
+          paddingLeft: Icon ? '20px' : '10px', 
+          paddingRight: `${dynamicPadding}px` 
+        }} 
+        {...props} 
+      />
+
+      <div className="search-right-container" ref={tagsRef}>
+        {/* Selected Color Search Swatch (no more than 1) */}
+        {selectedColors.slice(0, 1).map((color, idx) => (
           <div 
             key={`color-${idx}`} 
             className="input-tag-color" 
             style={{ backgroundColor: color }}
             onClick={() => onRemoveColor && onRemoveColor(color)}
-            title="Убрать цвет"
+            title="Remove color filter"
           />
         ))}
 
-        {selectedTags.map((tag, idx) => (
-          <div key={`tag-${idx}`} className="input-tag-text">
-            <span>{tag}</span>
-            <button 
-              type="button" 
-              onClick={() => onRemoveTag && onRemoveTag(tag)}
-            >
-              ×
-            </button>
-          </div>
+        {/* Selected tags in the input field (at most 2) */}
+        {selectedTags.slice(0, 2).map((tag, idx) => (
+          <Tag 
+            key={`tag-${idx}`} 
+            tag={tag} 
+            variant="input" 
+            onRemove={onRemoveTag} 
+          />
         ))}
+
+        {/* If more than 2 tags are selected, show the +n button */}
+        {selectedTags.length > 2 && (
+          <button 
+            type="button"
+            className="tag-more-button"
+            onClick={() => setIsTagManagerOpen(!isTagManagerOpen)}
+          >
+            +{selectedTags.length - 2}
+          </button>
+        )}
+
+        {/* Nested ColorPicker */}
+        {showColorPicker && (
+          <ColorPicker
+            color={pickerColor}
+            onChange={onPickerColorChange}
+            onOpenChange={setIsColorPickerOpen}
+            referenceEl={wrapperRef.current}
+          />
+        )}
       </div>
 
-      <input 
-        ref={inputRef}
-        className="splatera-input" 
-        style={{ paddingLeft: `${dynamicPadding}px` }} 
-        {...props} 
-      />
-      {Icon && <Icon size={16} className="input-icon" />}
+      {/* Tag manager dropdown menu */}
+      {isTagManagerOpen && (
+        <FloatingFocusManager context={context} modal={false} initialFocus={-1}>
+          <div 
+            ref={refs.setFloating} 
+            style={{ ...floatingStyles, zIndex: 10000 }} 
+            {...getFloatingProps()} 
+            className="extended-tag-dropdown"
+          >
+            {selectedTags.map((tag, idx) => (
+              <Tag 
+                key={`tag-dropdown-${idx}`} 
+                tag={tag} 
+                variant="dropdown" 
+                onRemove={onRemoveTag} 
+              />
+            ))}
+          </div>
+        </FloatingFocusManager>
+      )}
     </div>
   );
 
   if (tooltip || hotkey) {
     return (
-      <Tooltip content={tooltip || 'Search'} hotkey={hotkey} position={tooltipPosition}>
+      <Tooltip 
+        content={tooltip || 'Search'} 
+        hotkey={hotkey} 
+        position={tooltipPosition}
+        disabled={isTagManagerOpen || isColorPickerOpen}
+      >
         {inputContent}
       </Tooltip>
     );
